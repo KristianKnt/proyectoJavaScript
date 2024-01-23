37489fec1201dde62d6c026f515b24bd6e0ebff2
KristiaKnt <cristian.rojascapera5@gmail.com>
2024-01-22 23:12:46 -0500

    creating code in script.js and validationFomulario.js

--- cliente/js/script.js
+++ cliente/js/script.js
@@ -21,6 +21,14 @@ class RegistroEvento{
 
         const datosFormulario = this.obtenerDatosFormulario();
         const resultadoValidacion = validarDatosRegistroEvento(datosFormulario);
+
+        if(resultadoValidacion.esValido){
+            this.removerErroresCampo();
+            this.prepararEnvioDatos(datosFormulario);
+        }else{
+            this.removerErroresCampo();
+            this.resaltarCamposConErrores(resultadoValidacion.resultado);
+        }
     }
 
     obtenerDatosFormulario(){
@@ -30,10 +38,47 @@ class RegistroEvento{
             telefonoMoivl: this.telefonoMovil.value,
             edad: this.edad.value,
             profesion: this.profesion.value,
-            experiencia: this.experiencia.value,
+            experiencia: parseInt(document.querySelector('input[name="expetiencia"]:checked').value),
             expectativas: this.expectativas.value
             
 
         }
+    }
+
+    removerErroresCampo(){
+        this.nombre.parentElement.classList.remove('has-error');
+        this.email.parentElement.classList.remove('has-error');
+        this.telefonoMovil.parentElement.classList.remove('has-error');
+        his.edad.parentElement.classList.remove('has-error');
+        this.profesion.parentElement.classList.remove('has-error');
+        this.experiencia.parentElement.classList.remove('has-error');
+        
+    }
+
+    resaltarCamposConErrores(resultado){
+        if(!resultado.nombre){
+            this.nombre.parentElement.classList.add('has-error');
+        }
+        if(!resultado.email){
+            this.email.parentElement.classList.add('has-error');
+        }
+        if(!resultado.telefonoMoivl){
+            this.telefonoMovil.parentElement.classList.add('has-error');
+        }
+        if(!resultado.edadd){
+            this.edad.parentElement.classList.add('has-error');
+        }
+        if(!resultado.profesion){
+            this.profesion.parentElement.classList.add('has-error');
+        }
+        if(!resultado.experiencia){
+            this.experiencia.parentElement.classList.add('has-error');
+        }
+    }
+
+    prepararEnvioDatos(datosFormulario){
+        this.registrarEvento.classList.add('hidden');
+        this.indicadorCarga.classList.remove('hidden');
+        
     }
 }
--- cliente/js/validacionFormulario.js
+++ cliente/js/validacionFormulario.js
@@ -1,3 +1,50 @@
 function validarDatosRegistroEvento(datosFormulario){
-    
+    const resultado = {
+        nombre : validarNombre(datosFormulario.nombre),
+        email : validarEmail(datosFormulario.email),
+        telefonoMoivl: validarTelefonoMovil(datosFormulario.telefonoMoivl),
+        edad: validarEdad(datosFormulario.validarEdad),
+        profesion: validarProfesion(datosFormulario.profesion),
+        experiencia: validarExperiencia(datosFormulario.experiencia)        
+    }
+
+    let campo;
+    let esValido = true;
+
+    for(campo in resultado){
+        esValido = esValido && resultado[campo];
+
+    }
+    return {
+        esValido,
+        resultado
+    };
+}
+
+
+function validarNombre(nombre){
+    return nombre.length > 3;
+}
+
+function validarEmail(email){
+    const regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regexEmail.test(email);
+}
+
+function validarTelefonoMovil(telefonoMoivl){
+    const regexTelefonoMovil = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/; 
+    return regexTelefonoMovil.test(telefonoMoivl);
+}
+
+function validarEdad(edad){
+    return edad >= 10 && edad <= 50;
+}
+
+function validarProfesion(profesion){
+    const profesionesDisponibles =['estudiantes', 'desarrollador','ingeniero','otro'];
+    return profesionesDisponibles.indexOf(profesion) > -1;
+}
+
+function validarExperiencia(experiencia){
+    return experiencia >=1 && experiencia <= 3;
 }
